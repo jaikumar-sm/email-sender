6f692b41381ed7ebfc0dad863dfc86ea8ae3d37f
jaikumar-sm <jai.kumar@splashmath.com>
2020-04-20 13:06:11 +0530

    fix for email sender ingtegration with screenshot script

--- index.js
+++ index.js
@@ -1,47 +1,93 @@
 const express = require("express");
-const nodemailer = require('nodemailer'); 
-
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+global.SENT_EMAILS = [];
 var PORT = process.env.PORT || 3128;
 
 var app = express();
 var router = express.Router();
 
-router.get('/ping', function(req, res) {
-  res.json({ message: 'Its Alive!' });   
-});
-
-router.post('/send-mail', function(req, res) {
-  // console.log('req %s',req.query);
-  var toEmail = req.query.toEmail;
-  var subEmail = req.query.subEmail;
-  var bodyEmail = req.query.bodyEmail;
-
-  let mailTransporter = nodemailer.createTransport({ 
-    service: 'gmail', 
-    auth: { 
-        user: 'jai.kumar@splashlearn.com', 
-        pass: 'lexttvvpxkkefaau'
-    } 
+router.get('/ping', function (req, res) {
+  res.json({
+    message: 'Its Alive!'
   });
-  
-  let mailDetails = { 
-    from: 'jai.kumar@splashlearn.com', 
-    to: toEmail, 
-    subject: subEmail, 
-    text: bodyEmail
-  }; 
-  
-  mailTransporter.sendMail(mailDetails, function(err, data) { 
-    if(err) { 
-      console.log('Error Occurs');
-      res.send(err);
-    } else { 
-      console.log('Email sent successfully'); 
-      res.send(data);
-    } 
-  }); 
 });
 
+router.post('/send-mail', async (req, res, next) => {
+  try {
+    let {
+      toEmail,
+      subEmail,
+      bodyEmail
+    } = req.body;
+
+    if (!bodyEmail) {
+      return res.status(400).send({
+        "error": {
+          "message": " Email and Email Body is requied!"
+        }
+      });
+    }
+
+    if(!toEmail) {
+      toEmail = 'jai.kumar@splashlearn.com'
+    }
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'jai.kumar@splashlearn.com',
+        pass: ''
+      }
+    });
+
+    let mailOptions = {
+      from: 'jai.kumar@splashlearn.com',
+      to: toEmail,
+      subject: subEmail,
+      html: bodyEmail
+    };
+
+    if(isEmailSentAlready(bodyEmail)) {
+      return res.status(200).send({
+        "status": true,
+        "message": "Email Sent Already for this PT."
+      });
+    }
+    else {
+      transporter.sendMail(mailOptions, (error, data) => {
+        if (error) {
+          throw new error('Something went wrong')
+        }
+        return res.status(200).send({
+          "status": true,
+          "message": "Email Send Successfully."
+        });
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(200).send({
+      "error": {
+        "message": "Something went wrong"
+      }
+    });
+  }
+});
+
+function isEmailSentAlready(emailBody) {
+  console.log('return==>',global.SENT_EMAILS.indexOf(emailBody) >= 0 ? true : false);
+  if(global.SENT_EMAILS.indexOf(emailBody) >= 0) {
+    return true;
+  }
+  global.SENT_EMAILS.push(emailBody)
+  return false;
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api', router);
 
 app.listen(PORT, () => console.log(`Mailing server is up on port# : ${PORT}!`))
